491524cdbc9e9d6134ae4ad8f76612138a3ce103
goreleaserbot <goreleaser@carlosbecker.com>
2020-10-05 09:42:12 -0400

    Brew formula update for cirrus version v0.9.0

--- cirrus.rb
+++ cirrus.rb
@@ -2,16 +2,16 @@
 class Cirrus < Formula
   desc "CLI for running Cirrus Tasks locally in Docker containers"
   homepage "https://github.com/cirruslabs/cirrus-cli"
-  version "0.8.0"
+  version "0.9.0"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/cirruslabs/cirrus-cli/releases/download/v0.8.0/cirrus-darwin-amd64.tar.gz"
-    sha256 "19bb1536d1f6dbe37abce11553a4f0ba65da17292f5b5132afbb53cd4376ed5a"
+    url "https://github.com/cirruslabs/cirrus-cli/releases/download/v0.9.0/cirrus-darwin-amd64.tar.gz"
+    sha256 "c3498cec52ddc5cff88a779aa9b1303663fc87b251198dbf029abbdc8592dd72"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/cirruslabs/cirrus-cli/releases/download/v0.8.0/cirrus-linux-amd64.tar.gz"
-      sha256 "04e508826c3ba5351a3e1344c4b49b2c6ee85697b399f6056fbe926d1f668e0e"
+      url "https://github.com/cirruslabs/cirrus-cli/releases/download/v0.9.0/cirrus-linux-amd64.tar.gz"
+      sha256 "e523cadb9c810fa49b0cf37dc622a047d7936d58790f38b04836a89dd9ec9ff3"
     end
   end
 
