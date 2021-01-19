ad427aa050815189b19368d60be60d445953af20
goreleaserbot <goreleaser@carlosbecker.com>
2021-01-19 10:20:51 -0500

    Brew formula update for cirrus version v0.30.1

--- cirrus.rb
+++ cirrus.rb
@@ -5,24 +5,24 @@
 class Cirrus < Formula
   desc "CLI for running Cirrus Tasks locally in Docker containers"
   homepage "https://github.com/cirruslabs/cirrus-cli"
-  version "0.30.0"
+  version "0.30.1"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/cirruslabs/cirrus-cli/releases/download/v0.30.0/cirrus-darwin-amd64.tar.gz"
-    sha256 "e3bc9751f24e903b8c52b49454af6b6962683a30732c09d881a2790f24eeb295"
+    url "https://github.com/cirruslabs/cirrus-cli/releases/download/v0.30.1/cirrus-darwin-amd64.tar.gz"
+    sha256 "7ea6a0fefe730c2419a3810df5f8117cf9892e66f18393da13a129e67f075c7e"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/cirruslabs/cirrus-cli/releases/download/v0.30.0/cirrus-linux-amd64.tar.gz"
-    sha256 "1b62a0de9515b87d85c03d89674fde7e9d7d59e9b6a7891de86039b4add2674b"
+    url "https://github.com/cirruslabs/cirrus-cli/releases/download/v0.30.1/cirrus-linux-amd64.tar.gz"
+    sha256 "0b07c58f1fabe45771ed967e9c381b24e547631af8fda9a75d9dc6e691173c25"
   end
   if OS.linux? && Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-    url "https://github.com/cirruslabs/cirrus-cli/releases/download/v0.30.0/cirrus-linux-arm.tar.gz"
-    sha256 "6d98cfc37785d5c4fbb748ffd32787ac36dc8bf5ee1b1ec63f84429d7fdddbc4"
+    url "https://github.com/cirruslabs/cirrus-cli/releases/download/v0.30.1/cirrus-linux-arm.tar.gz"
+    sha256 "f3e0df444b40702eba16ee6e8ca8c0ba4fe0f4d7b24aef2e125970c1ca171da0"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/cirruslabs/cirrus-cli/releases/download/v0.30.0/cirrus-linux-arm64.tar.gz"
-    sha256 "076f3c7f2b7fdf900f829366f9ce2c95eef2588bc1dbfc201fc0a7da821df8d9"
+    url "https://github.com/cirruslabs/cirrus-cli/releases/download/v0.30.1/cirrus-linux-arm64.tar.gz"
+    sha256 "49bdbe5cc327c344a061869c7fb8b2047afbd07da7007201286708740be6ef8d"
   end
 
   def install
