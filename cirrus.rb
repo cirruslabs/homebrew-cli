e3f97445a78158e772989759da5c70157b84bb8d
goreleaserbot <goreleaser@carlosbecker.com>
2021-01-29 14:46:05 -0500

    Brew formula update for cirrus version v0.31.1

--- cirrus.rb
+++ cirrus.rb
@@ -5,24 +5,24 @@
 class Cirrus < Formula
   desc "CLI for running Cirrus Tasks locally in Docker containers"
   homepage "https://github.com/cirruslabs/cirrus-cli"
-  version "0.31.0"
+  version "0.31.1"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/cirruslabs/cirrus-cli/releases/download/v0.31.0/cirrus-darwin-amd64.tar.gz"
-    sha256 "4628f1020296d383a163368e353e466806d9f2f86b662515c32d27cd09987e6e"
+    url "https://github.com/cirruslabs/cirrus-cli/releases/download/v0.31.1/cirrus-darwin-amd64.tar.gz"
+    sha256 "b84532f5b8797a3452418b21487738166cf70a6c8a8eca274f0fd57fa75e54ba"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/cirruslabs/cirrus-cli/releases/download/v0.31.0/cirrus-linux-amd64.tar.gz"
-    sha256 "fb543e5440aaaaa66a1a1cfc27146fddf66da9436c2b0a260702fff809438413"
+    url "https://github.com/cirruslabs/cirrus-cli/releases/download/v0.31.1/cirrus-linux-amd64.tar.gz"
+    sha256 "b4db5c5d0e9073623885014537e21da705b20de4ec951b8af5380eaeecf259df"
   end
   if OS.linux? && Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-    url "https://github.com/cirruslabs/cirrus-cli/releases/download/v0.31.0/cirrus-linux-arm.tar.gz"
-    sha256 "a58256fbbeffc1cc204e4868f4b6366681a5260e44c3dbafae06a3374b23201b"
+    url "https://github.com/cirruslabs/cirrus-cli/releases/download/v0.31.1/cirrus-linux-arm.tar.gz"
+    sha256 "090ef40d6c6146e23f2909b713cb6ecfc68cdf4f5fd0f76de0bdf18dc4684cff"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/cirruslabs/cirrus-cli/releases/download/v0.31.0/cirrus-linux-arm64.tar.gz"
-    sha256 "4d064925587b44e00ccdb07898f51cceda58b0390dcc373aa44c058017340386"
+    url "https://github.com/cirruslabs/cirrus-cli/releases/download/v0.31.1/cirrus-linux-arm64.tar.gz"
+    sha256 "2c514ad8e5b74ea3f850046617b9602bd903021b17c32d313c997bf5776d1ed7"
   end
 
   def install
