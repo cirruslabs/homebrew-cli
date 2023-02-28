40d0233526748c06a446c75ed36872433a4ba55c
fedor <fedor.korotkov@gmail.com>
2023-02-28 13:08:37 -0500

    Revert "Brew formula update for tart version 1.0.0"
    This reverts commit b2d1b5aae51d01c5b2f239da33436894c524a956.

--- tart.rb
+++ tart.rb
@@ -5,20 +5,19 @@
 class Tart < Formula
   desc "Run macOS VMs on Apple Silicon"
   homepage "https://github.com/cirruslabs/tart"
-  version "1.0.0"
-  license "Fair Source"
+  version "0.38.0"
+  license "AGPL-3.0"
 
   depends_on "cirruslabs/cli/softnet"
   depends_on :macos
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/cirruslabs/tart/releases/download/1.0.0/tart.tar.gz"
-      sha256 "7c4c469fd89423a943de10c3ba6ea8e14d5e718503fa8045b2fa8e34996ee84f"
+      url "https://github.com/cirruslabs/tart/releases/download/0.38.0/tart.tar.gz"
+      sha256 "0870ca88e53c92f61372288af4a4caaac40cc23358fd82006e9ab9f63b3eac0a"
 
       def install
-        libexec.install Dir["*"]
-        bin.write_exec_script "#{libexec}/Tart.app/Contents/MacOS/tart"
+        bin.install "tart"
       end
     end
   end
